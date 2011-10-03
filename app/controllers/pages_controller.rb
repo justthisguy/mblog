93d4c48b0d2353acb09d76ff6a24c59fdb31372c
Ken Decanio <ken-decanio@41monkeys>
2011-10-02 20:19:51 -0700

    Done with static-pages and chapter 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
 class PagesController < ApplicationController
+	
+	def init
+		@main_title = 'Ruby on Rails Tutorial Sample App'
+		@page_title = ''
+	end
+	
   def home
+		@page_title = 'Home'
   end
 
   def contact
+		@page_title = 'Contact'
   end
 
 	def about
-		
+		@page_title = 'About'
 	end
 
 end
